1b11d85bda19602701a0688937e9acc55ac4abca
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 17:57:22 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "ansible_private_key" {}
+
+variable "ansible_host" {}
+
+variable "server_public_key" {}
+
+variable "instance_count" {
+  default = 3
+  type    = number
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
