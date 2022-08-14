521a761c6bd10a982f1ba36405a81360651bf3c1
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 18:07:44 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ resource "null_resource" "ansible-server" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -u ec2-user -T 60 -b -i ${join(",", aws_instance.web-server[*].public_ip)}, /home/ec2-user/playbooks/apache.yaml",
+      "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -u ec2-user -b -i ${join(",", aws_instance.web-server[*].public_ip)}, /home/ec2-user/playbooks/apache.yaml",
     ]
   }
 }
