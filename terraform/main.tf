d7078c7cf69037e1a33411e79e2d5d79bf73564d
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 13:57:15 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,6 @@ terraform {
     }
   }
 }
-/*
-  test
-*/
 
 resource "aws_instance" "web-server-1" {
   ami             = "ami-0460bf124812bebfa"
@@ -18,4 +15,17 @@ resource "aws_instance" "web-server-1" {
   tags = {
     Name = "web-server-1"
   }
+}
+
+resource "null_resource" "ansible-server" {
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "${var.ansible_private_key}"
+    host        = "${var.ansible_host}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -b --private-key ${var.ansible_private_key} -i ${aws_instance.web-server-1.public_ip} /home/ec2-user/playbooks/hardening-playbook.yaml",
+    ]
 }
