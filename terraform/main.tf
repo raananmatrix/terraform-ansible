80f94f5b160d25f6f04f0928fcfb8223ac1e17db
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 16:31:09 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,18 +10,19 @@ terraform {
 variable "ansible_private_key" {}
 variable "ansible_host" {}
 
-resource "aws_instance" "web-server-1" {
+resource "aws_instance" "web-server" {
+  count           = 2
   ami             = "ami-0460bf124812bebfa"
   instance_type   = "t2.micro"
   security_groups =  [ "MyWebSG" ]
   key_name        = "paris-key"
-  tags = {
-    Name = "web-server-1"
+  tags            = {
+    Name = "web-server-${count.index}"
   }
 }
 
 resource "null_resource" "ansible-server" {
-  depends_on = [aws_instance.web-server-1]
+  depends_on = [aws_instance.web-server]
   connection {
     type        = "ssh"
     user        = "ec2-user"
@@ -30,7 +31,7 @@ resource "null_resource" "ansible-server" {
   }
   provisioner "remote-exec" {
     inline = [
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -b -i ${aws_instance.web-server-1.public_ip}, /home/ec2-user/playbooks/apache.yaml",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -b -i ${join(',' aws_instance.web-server[*].public_ip)}, /home/ec2-user/playbooks/apache.yaml",
     ]
   }
 }
