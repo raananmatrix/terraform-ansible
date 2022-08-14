f37cc89eee8f2fe36c926dbe7af02ebafca05981
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 12:08:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+
+resource "aws_instance" "web-server-1" {
+  ami             = "ami-0460bf124812bebfa"
+  instance_type   = "t2.micro"
+  security_groups =  [ "MyWebSG" ]
+  key_name        = "paris-key"
+  tags = {
+    Name = "web-server-1"
+  }
+}
