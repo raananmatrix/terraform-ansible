2a7b0562b06d680b5d5ae7adcab6406f82d639e7
raananmatrix <97457371+raananmatrix@users.noreply.github.com>
2022-08-14 18:09:40 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,11 +27,13 @@ resource "aws_security_group" "allow_http_and_ssh" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
     Name = "allow_8080_and_ssh"
