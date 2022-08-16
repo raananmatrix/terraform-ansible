70feb65956046a2840bcbaad8dfed83b471e7977
raananmatrix <raananmatrix@gmail.com>
2022-08-16 16:15:15 +0300

    listener protocol

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -22,7 +22,7 @@ resource "aws_lb_target_group_attachment" "web-servers-attachment" {
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.web-servers-lb.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "TCP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web-servers-target-group.arn
